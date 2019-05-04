ed1ccb0227fdf5d6702377f979cc2f882369ac7d
Marek Jaczewski <jaczewski147@gmail.com>
2019-05-04 11:29:04 +0200

    move main method to Main class

--- src/com/mj147/Main.java
+++ src/com/mj147/Main.java
@@ -1,4 +1,10 @@
 package com.mj147;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        ticTacToeGame.fillTable();
+    }
 }
